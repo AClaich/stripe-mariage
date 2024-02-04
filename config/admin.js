275a7466821ef17bb9e624eb5857b438b9b2c07e
AClaich <claich.alexis@gmail.com>
2024-02-04 13:07:31 +0100

    New environnement

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', '5bGaWCKgRV9VuPfwr8l+xg=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'RoiWJVwkvhqpuGFD+sIHRw=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'E4fynCu4El5u7hDENcBCbw=='),
     },
   },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
+  // flags: {
+  //   nps: env.bool('FLAG_NPS', true),
+  //   promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  // },
 });
--- config/server.js
+++ config/server.js
@@ -1,10 +1,16 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  url: env('STRAPI_URL', 'https://www.mariage-alexis-margaux.site/'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5bGaWCKgRV9VuPfwr8l+xg=='),
+    },
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+  // app: {
+  //   keys: env.array('APP_KEYS'),
+  // },
+  // webhooks: {
+  //   populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  // },
 });
