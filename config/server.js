0540f84fc938300d1b2d9464adf1d25eac785bfc
AClaich <claich.alexis@gmail.com>
2024-02-04 23:02:48 +0100

    port 1337

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 4200),
+  port: env.int('PORT', 1337),
   // url: env('STRAPI_URL', 'https://www.mariage-alexis-margaux.site/'),
   admin: {
     auth: {
